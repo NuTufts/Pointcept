2480519196d008c64fd85e58a2fb5ffb4f21c809
twongjirad <taritree.wongjirad@gmail.com>
2025-12-05 04:32:02 -0500

    add script files for preprocessing dlmerged files into hdf5 files

---
+++ lartpc_data_prep/submit_bnbnu_pi0_corsika.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=dl2h5
+#SBATCH --output=logs/bnbnu_pi0_corsika/gridlog_dl2h5.%j.%A_%a.%N.log
+#SBATCH --mem-per-cpu=8000
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=wongjiradlab
+#SBATCH --error=logs/bnbnu_pi0_corsika/griderr_dl2h5.%j.%A_%a.%N.err
+#SBATCH --array=0-9
+
+# set the location of your copy of the repo here
+WORKDIR=/cluster/tufts/wongjiradlabnu/twongj01/pointcept_env/pointcept/lartpc_data_prep/
+
+# location of the sl7 container here
+container=/cluster/tufts/wongjiradlabnu/larbys/larbys-container/pointcept.sif
+
+# setup singularity on the node
+module load apptainer/1.2.4-suid
+
+# run job script inside container
+apptainer exec --bind /cluster:/cluster $container bash -c "cd ${WORKDIR} && source run_corsika_bnb_nu_pi0.sh"
+
