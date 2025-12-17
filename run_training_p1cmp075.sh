244abbe947919714e19ec769645ccbc2a24ca7a0
twongjirad <taritree.wongjirad@gmail.com>
2025-12-17 10:13:39 -0500

    add files before lunching v2 training

---
+++ run_training_p1cmp075.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /cluster/tufts/wongjiradlabnu/twongj01/pointcept_env/
+source setenv_pointcept_only.sh
+cd /cluster/tufts/wongjiradlabnu/twongj01/pointcept_env/pointcept
+
+python3 tools/train.py --config configs/lartpc/pretrain-sonata-v1m1-lartpc-v2.py --num-gpus 4
+##python3 tools/train.py --config configs/lartpc/pretrain-sonata-v1m1-lartpc-restart-p100.py --options weight=/cluster/tufts/wongjiradlabnu/twongj01/pointcept_env/pointcept/epoch_87.pth epoch=200 eval_epoch=200
+
