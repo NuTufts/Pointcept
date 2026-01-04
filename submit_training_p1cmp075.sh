69e334a96610e791aee9d56dcb05ea332e8afa11
twongjirad <taritree.wongjirad@gmail.com>
2026-01-04 15:38:37 -0500

    save before starting pretrain v3

--- submit_training_p1cmp075.sh
+++ submit_training_p1cmp075.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=larsonata
-#SBATCH --output=larsonata.%j.%N.log
+#SBATCH --output=larsonata.v3.%j.%N.log
 #SBATCH --mem-per-cpu=4000
 #SBATCH --cpus-per-task=10
 #SBATCH --time=6-00:00:00
 #SBATCH --partition=wongjiradlab
 #SBATCH --gres=gpu:p100:4
-#SBATCH --error=larsonata.%j.%N.err
+#SBATCH --error=larsonata.v3.%j.%N.err
 
 # set the location of your copy of the repo here
 WORKDIR=/cluster/tufts/wongjiradlabnu/twongj01/pointcept_env/pointcept/
