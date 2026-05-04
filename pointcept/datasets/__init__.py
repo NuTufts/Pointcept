9ea5be79987ffeb1d8faa09eb6d45ec87af9ac9c
twongjirad <taritree.wongjirad@gmail.com>
2026-05-04 09:48:11 -0400

    add missing init line for shower_clustering dataset

--- pointcept/datasets/__init__.py
+++ pointcept/datasets/__init__.py
@@ -23,6 +23,7 @@ from .shapenet_part import ShapeNetPartDataset
 # physics / LArTPC
 from .lartpc import LArTPCDataset, LArTPCInstanceDataset
 from .shower_origin import ShowerOriginDataset, ShowerOriginDatasetSimple
+from .shower_clustering import ShowerClusteringDataset, shower_clustering_collate
 
 # dataloader
 from .dataloader import MultiDatasetDataloader
